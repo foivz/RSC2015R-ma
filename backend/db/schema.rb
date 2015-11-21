19f0af81efce2591f425869650c85b7f62271c3a
Dinko Osrecki <dinko.osrecki@gmail.com>
2015-11-21 18:12:23 +0100

    Add active column to games table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121170551) do
+ActiveRecord::Schema.define(version: 20151121171133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,9 @@ ActiveRecord::Schema.define(version: 20151121170551) do
     t.integer  "field_id"
     t.integer  "team_a_id"
     t.integer  "team_b_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.boolean  "active",     default: true
   end
 
   create_table "obstacles", force: :cascade do |t|
