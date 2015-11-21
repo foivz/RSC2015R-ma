c74116731afa09d9d2503112116e79ce85aaa0f8
Dinko Osrecki <dinko.osrecki@gmail.com>
2015-11-21 15:17:55 +0100

    Fix routes7

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :authentication do
       post :login, :logout
+    end
 
-      resources :users, only: [:index, :show, :create, :update, :destroy] do
-        get :me, on: :collection
-      end
+    resources :users, only: [:index, :show, :create, :update, :destroy] do
+      get :me, on: :collection
     end
   end
 end
