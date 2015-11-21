030740cfbaada6f92a56c58be029c60b69f724f8
Dinko Osrecki <dinko.osrecki@gmail.com>
2015-11-21 18:04:36 +0100

    Create obstacles table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121165829) do
+ActiveRecord::Schema.define(version: 20151121170339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20151121165829) do
     t.integer  "team_b_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "obstacles", force: :cascade do |t|
+    t.decimal  "latitude",   precision: 8, scale: 6
+    t.decimal  "longitude",  precision: 9, scale: 6
+    t.integer  "type"
+    t.integer  "game_id"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
   end
 
   create_table "teams", force: :cascade do |t|
