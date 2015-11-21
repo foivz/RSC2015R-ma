4ff49b4044c0d5fc1a06349055fae84be7183078
Dinko Osrecki <dinko.osrecki@gmail.com>
2015-11-21 13:57:34 +0100

    Setup authentication and users routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   # Api
   namespace :api, defaults: { format: :json } do
+    namespace :authentication do
+      post :login, :logout
 
+      resources :users, only: [:index, :show, :create, :update, :destroy]
+    end
   end
 end
