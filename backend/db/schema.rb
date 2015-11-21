32943a193562a0b9a5d345c6fcfcbd499239753c
Dinko Osrecki <dinko.osrecki@gmail.com>
2015-11-21 20:29:33 +0100

    Add indiviudal messages

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121183027) do
+ActiveRecord::Schema.define(version: 20151121192708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20151121183027) do
     t.boolean  "active",     default: false
     t.datetime "start_date"
     t.integer  "judge_id"
+  end
+
+  create_table "individual_messages", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.text     "message"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "obstacles", force: :cascade do |t|
