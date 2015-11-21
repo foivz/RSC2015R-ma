434575bf1400218149ad408133ffd92e5d659469
Dinko Osrecki <dinko.osrecki@gmail.com>
2015-11-22 00:42:19 +0100

    Disable cors

--- backend/app/controllers/api/api_base_controller.rb
+++ backend/app/controllers/api/api_base_controller.rb
@@ -8,4 +8,13 @@ class Api::ApiBaseController < ApplicationController
   before_action :check_access_token
 
   skip_before_action :verify_authenticity_token
+
+  before_filter :disable_cors
+
+  def disable_cors
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 end
