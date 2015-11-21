9987beade4bfa8e3c78f2391b46d975d946bf13a
Dinko Osrecki <dinko.osrecki@gmail.com>
2015-11-21 18:40:24 +0100

    Add judge to games

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121171519) do
+ActiveRecord::Schema.define(version: 20151121173948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20151121171519) do
     t.datetime "updated_at",                null: false
     t.boolean  "active",     default: true
     t.datetime "start_date"
+    t.integer  "judge_id"
   end
 
   create_table "obstacles", force: :cascade do |t|
