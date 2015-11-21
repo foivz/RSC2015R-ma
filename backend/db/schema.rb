7a4f6107ba9b658700aadcdf531bcfc385a90b5f
Dinko Osrecki <dinko.osrecki@gmail.com>
2015-11-21 20:33:31 +0100

    Add team messages

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121192708) do
+ActiveRecord::Schema.define(version: 20151121193208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,14 @@ ActiveRecord::Schema.define(version: 20151121192708) do
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
     t.integer  "team_id"
+  end
+
+  create_table "team_messages", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "teams", force: :cascade do |t|
