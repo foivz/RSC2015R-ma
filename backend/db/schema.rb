97f4e7f2113789280931e4a2fa742e303adf99fd
Dinko Osrecki <dinko.osrecki@gmail.com>
2015-11-22 01:30:26 +0100

    Add active column to team

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121223113) do
+ActiveRecord::Schema.define(version: 20151122002936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,8 +77,9 @@ ActiveRecord::Schema.define(version: 20151121223113) do
     t.decimal  "latitude",   precision: 8, scale: 6
     t.decimal  "longitude",  precision: 9, scale: 6
     t.integer  "score"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.boolean  "active",                             default: true
   end
 
   create_table "users", force: :cascade do |t|
